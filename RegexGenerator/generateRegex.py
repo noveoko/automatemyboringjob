8b3f39162fc47d6d00623c74e67dd3139f40b0ed
Marcin Kraszewski <visitwarsaw@gmail.com>
2021-08-31 09:14:01 +0200

    fixed error with generate_regex_from_string

--- RegexGenerator/generateRegex.py
+++ RegexGenerator/generateRegex.py
@@ -13,30 +13,33 @@ class generateRegex:
 
     seed = 0.2323
 
-    def __init__(self, string: str, negative_string: str):
+    def __init__(self, string: str = "abc", negative_string: str = "10"):
         self.string = string
         self.negative_string = negative_string
         self.max_patterns = 5
         self.max_tries = 1000
         self.found_patterns = set()
         self.pattern_size: int = 3
-        self.unique_patterns: int = 1
+        self.unique_patterns: int = 100
         self.symbol_map = uf.create_probability_object(uf)
 
     def generate_regex_pattern(self):
         all_patterns = []
+        random.seed(self.seed)
         while len(all_patterns) < self.unique_patterns:
-            random.seed(self.seed)
-            pattern = r""
-            while len(pattern) < self.pattern_size:
-                choice = random.choices(
-                    [a for a in self.symbol_map.keys()],
-                    [a for a in self.symbol_map.values()],
-                    k=1,
-                )
-                random_symbol = random.choice(choice[0])
-                pattern += random_symbol
-            all_patterns.append([self.pattern_size, pattern])
+            try:
+                pattern = r""
+                while len(pattern) < self.pattern_size:
+                    choice = random.choices(
+                        [a for a in self.symbol_map.keys()],
+                        [a for a in self.symbol_map.values()],
+                        k=1,
+                    )
+                    random_symbol = random.choice(choice[0])
+                    pattern += random_symbol
+                all_patterns.append([self.pattern_size, pattern])
+            except IndexError as ie:
+                print(ie)
         return all_patterns
 
     def generate_from_regex(
@@ -46,19 +49,30 @@ class generateRegex:
         failed_matches = 0
         tries = 0
         while not matching_string and tries < max_tries:
-            symbols_count = random.choice(range(1, max_symbols))
-            string = ""
-            while len(string) < symbols_count:
-                if self.symbol_map:
-                    string_choice = random.choice([a for a in self.symbol_map.keys()])
-                    string += string_choice
-            if re.match(pattern, string):
-                matching_string.add(string)
-            else:
-                failed_matches += 1
-                print(f"Searching for: {pattern} {failed_matches}", end="\r")
-            tries += 1
-        return [matching_string, pattern, failed_matches]
+            try:
+                symbols_count = random.choice(range(1, max_symbols))
+                string = ""
+                while len(string) < symbols_count:
+                    print(string)
+                    if self.symbol_map:
+                        string_choice = random.choice(
+                            [a for a in self.symbol_map.keys()]
+                        )
+                        string += string_choice
+                    if re.match(pattern, string):
+                        matching_string.add(string)
+                        break
+                    else:
+                        failed_matches += 1
+                        print(f"Searching for: {pattern} {failed_matches}", end="\r")
+                tries += 1
+            except Exception as e:
+                print(e)
+        else:
+            shortest_matching_string = sorted(matching_string, key=len)[0]
+            package = [shortest_matching_string, pattern, failed_matches]
+            print(f"Done! {package}")
+            return package
 
     def best_pattern(self):
         return sorted(
@@ -80,20 +94,24 @@ class generateRegex:
         tries = 0
         while tries < self.max_tries:
             try:
-                new_pattern = self.generate_regex_pattern(unique_patterns=1000)
-                for pattern in new_pattern:
-                    pattern = pattern[1]
-                    print(pattern)
-                    if re.fullmatch(pattern, self.string):
-                        self.found_patterns.add(pattern)
-                        print(
-                            f"Pattern found! {pattern} found:{len(self.found_patterns)}",
-                            end="\r",
-                        )
-            except Exception as ee:
-                print(ee)
-            finally:
                 tries += 1
+                new_pattern = self.generate_regex_pattern()
+                for pattern in new_pattern:
+                    try:
+                        pattern = pattern[1]
+                        assert isinstance(self.string, str)
+                        if re.match(pattern, self.string):
+                            self.found_patterns.add(pattern)
+                            print(
+                                f"Pattern match found! {pattern} found:{len(self.found_patterns)}",
+                                end="\r",
+                            )
+                    except Exception as ee:
+                        pass
+            except AssertionError as ae:
+                print("2a:", ae)
+            except Exception as ee:
+                print("2: ", ee)
         if self.negative_string:
             self.found_patterns = self.best_pattern()
 
