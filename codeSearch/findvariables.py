e7e177537bb08f8c0d5b32237d8561a90b6dca9a
Marcin Kraszewski <visitwarsaw@gmail.com>
2023-11-29 09:46:39 +0100

    Update findvariables.py

--- codeSearch/findvariables.py
+++ codeSearch/findvariables.py
@@ -1,13 +1,4 @@
-To extract all variable names from a Python project, you will first need to parse the Python code files. This can be done using the `ast` module (Abstract Syntax Trees) in Python, which can analyze and interpret the structure of Python code.
-
-Here's a Python script that can do this:
-
-1. It will search through all Python files in a given directory.
-2. It will parse each file to find all the variable names.
-
-This script assumes that you have a folder structure where all your Python files are in a single directory or its subdirectories.
-
-```python
+python
 import ast
 import os
 
@@ -19,8 +10,10 @@ class VariableVisitor(ast.NodeVisitor):
         if isinstance(node.ctx, ast.Store):
             self.variables.add(node.id)
 
-def find_python_files(directory):
+def find_python_files(directory, ignore_dirs):
     for root, dirs, files in os.walk(directory):
+        # Modify the dirs list in-place to skip ignored directories
+        dirs[:] = [d for d in dirs if d not in ignore_dirs]
         for file in files:
             if file.endswith('.py'):
                 yield os.path.join(root, file)
@@ -33,19 +26,15 @@ def extract_variables_from_file(file_path):
     visitor.visit(tree)
     return visitor.variables
 
-def main(directory):
+def main(directory, ignore_dirs):
     all_variables = set()
-    for file_path in find_python_files(directory):
+    for file_path in find_python_files(directory, ignore_dirs):
         variables = extract_variables_from_file(file_path)
         all_variables.update(variables)
     return all_variables
 
 # Replace 'your_project_directory' with the path to your Python project directory
 project_directory = 'your_project_directory'
-variables = main(project_directory)
+ignore_dirs = ['dir_to_ignore1', 'dir_to_ignore2']  # Replace with directories to ignore
+variables = main(project_directory, ignore_dirs)
 print(variables)
-```
-
-Remember to replace `'your_project_directory'` with the actual path to your project directory. This script will print all the variable names used in your Python project.
-
-Keep in mind that this script identifies variable names based on the `ast.Store` context, which means it catches variables that are being assigned values. It may not catch every possible usage of a variable (like in global statements or as function arguments).
