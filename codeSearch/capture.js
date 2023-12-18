e151d2f84c83eefd688c7b2b5870235fe59c4813
Marcin Kraszewski <visitwarsaw@gmail.com>
2023-12-18 17:03:20 +0100

    Update capture.js

--- codeSearch/capture.js
+++ codeSearch/capture.js
@@ -1,1 +1,24 @@
-R
+Here is the most relevant sentence in the context: To capture a `div` that appears after another element is clicked, you can use the event delegation approach by attaching an event listener to a common parent of both elements.
+
+```javascript
+// Assuming there is a common parent element with id "commonParent"
+const commonParent = document.getElementById('commonParent');
+
+// Add click event listener to the common parent
+commonParent.addEventListener('click', function(event) {
+  // Check if the clicked element is the trigger element
+  if (event.target.id === 'yourTriggerElementId') {
+    // Your code to handle the appearance of the div
+    console.log('Trigger element clicked, capture the dynamically created div');
+    
+    // Access the dynamically created div (replace 'yourDynamicDivId' accordingly)
+    const dynamicallyCreatedDiv = document.getElementById('yourDynamicDivId');
+    if (dynamicallyCreatedDiv) {
+      // Your code to handle the dynamically created div
+      console.log('Captured the dynamically created div:', dynamicallyCreatedDiv);
+    }
+  }
+});
+```
+
+Replace `'yourTriggerElementId'` and `'yourDynamicDivId'` with the actual IDs of the trigger element and the dynamically created `div`. This way, you can capture the click event on the common parent and check if the clicked element is the trigger element.
